6bc36098d4f1fd61472b774af1ade79f69fc5b24
Hugo Chou <hugo@beansmile.com>
2020-08-08 20:22:06 +0800

    part6 | redux-anecdotes exercises 6.13 - 6.14

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import anecdoteService from './services/anecdotes'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <Notification />
